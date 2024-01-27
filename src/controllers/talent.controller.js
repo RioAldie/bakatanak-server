d34ffbadecb7d8a29e92ea09d614a112ffe2266f
rioaldie <rioaldierwanto@gmail.com>
2024-01-27 07:02:05 +0700

    make a save consultation result

--- src/controllers/talent.controller.js
+++ src/controllers/talent.controller.js
@@ -1,11 +1,41 @@
 const { CertaintyFactor } = require('../service/cf');
+const Consult = require('../models/consult');
 
 const getTalentIdentification = async (req, res) => {
-  const userIndicatorValue = req.body.indicator;
+  try {
+    const userIndicatorValue = req.body.indicator;
 
-  const result = CertaintyFactor(userIndicatorValue);
+    const result = CertaintyFactor(userIndicatorValue);
 
-  res.status(200).json({ message: result });
+    res.status(200).json({ message: result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = { getTalentIdentification };
+const saveConsultResult = async (req, res) => {
+  try {
+    const { result, name, age, school, city, userId } = req.body;
+
+    const date = new Date();
+    let now = date.toLocaleDateString('fr-CA');
+
+    const newResult = await Consult({
+      date: now,
+      result,
+      name,
+      age,
+      school,
+      city,
+      userId,
+    });
+
+    await newResult.save();
+
+    return res.status(200).json({ message: 'save result success' });
+  } catch (error) {
+    res.status(500).json({ message: 'error' });
+  }
+};
+
+module.exports = { getTalentIdentification, saveConsultResult };
--- src/routes/talent.router.js
+++ src/routes/talent.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
   getTalentIdentification,
+  saveConsultResult,
 } = require('../controllers/talent.controller');
 
 const talentRouter = express.Router();
 
 talentRouter.post('/', getTalentIdentification);
+talentRouter.post('/save', saveConsultResult);
 
 module.exports = talentRouter;
