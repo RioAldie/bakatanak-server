914f152ce45a660f8d9194b853988ce92792fd72
rioaldie <rioaldierwanto@gmail.com>
2024-01-30 06:37:53 +0700

    connect with atlas

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const database_url = 'mongodb://127.0.0.1:27017/db_talentexpert';
 
-const database = mongoose.connect(database_url);
+const database = mongoose.connect(process.env.MONGODB_CONNECT_URI);
 
 module.exports = database;
